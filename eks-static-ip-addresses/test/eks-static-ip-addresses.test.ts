5fdf3ed155b811c25cd8f9f078272af198ad5945
Michael CC <333mmcc@gmail.com>
2023-10-15 23:29:22 +1000

    Added eks-static-ip-addresses

---
+++ eks-static-ip-addresses/test/eks-static-ip-addresses.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EksStaticIpAddresses from '../lib/eks-static-ip-addresses-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-static-ip-addresses-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksStaticIpAddresses.EksStaticIpAddressesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
