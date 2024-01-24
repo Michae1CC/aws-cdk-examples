b10699cc464239e5d1592268b7f3aa8cdac84497
Michael CC <333mmcc@gmail.com>
2024-01-24 18:58:15 +1000

    Add styling to list

--- cognito-web-login/lib/coffee-app/app/routes/_index.tsx
+++ cognito-web-login/lib/coffee-app/app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction, LinksFunction } from "@remix-run/node";
 
 import stylesUrl from "~/styles/index.css";
 import coffeeImage from "~/img/coffee_image.png";
+import coffeeImage2 from "~/img/coffee_image2.png";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -36,47 +37,50 @@ export default function Index() {
         <div className="featured-picture-container">
           <div className="featured-picture">
             <img src={coffeeImage} alt={"Coffee"}></img>
-            <h2>
-              Pouring the Perfect Latte Art
-            </h2>
+            <h2>Pouring the Perfect Latte Art</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius purus. Cras at aliquam est, quis ultricies risus.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et
+              venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius
+              purus. Cras at aliquam est, quis ultricies risus.
+            </p>
+          </div>
+          <div className="featured-picture">
+            <img src={coffeeImage2} alt={"Coffee"}></img>
+            <h2>Different Styles of Coffee</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et
+              venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius
+              purus. Cras at aliquam est, quis ultricies risus.
             </p>
           </div>
           <div className="featured-picture">
             <img src={coffeeImage} alt={"Coffee"}></img>
-            <h2>
-              Pouring the Perfect Latte Art
-            </h2>
+            <h2>Pouring the Perfect Latte Art</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius purus. Cras at aliquam est, quis ultricies risus.
-            </p>
-          </div>
-          <div className="featured-picture">
-            <img src={coffeeImage} alt={"Coffee"}></img>
-            <h2>
-              Pouring the Perfect Latte Art
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius purus. Cras at aliquam est, quis ultricies risus.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et
+              venenatis risus. Integer at ipsum vehicula, laoreet enim a, varius
+              purus. Cras at aliquam est, quis ultricies risus.
             </p>
           </div>
         </div>
         <div className="featured-list">
-          <h3>
-            Featured
-          </h3>
+          <h3>Featured</h3>
           <hr />
-          <ul>
-            <li className="featured-list-item">
-              <h4>
-                Title
-              </h4>
-              <p>
-                Description
-              </p>
-            </li>
-          </ul>
+          <div className="featured-list-item">
+            <h4>Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing edivt. Nam et
+              venenatis risus.
+            </p>
+          </div>
+          <hr />
+          <div className="featured-list-item">
+            <h4>Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing edivt. Nam et
+              venenatis risus.
+            </p>
+          </div>
         </div>
       </div>
     </div>
