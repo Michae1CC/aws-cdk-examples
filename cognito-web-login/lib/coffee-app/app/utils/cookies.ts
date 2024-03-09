714623d5b7f6bff7556ec95a6fdafa34b391ae38
Michael CC <333mmcc@gmail.com>
2024-03-09 22:19:43 +1000

    rm old folder

--- cognito-web-login/lib/coffee-app/app/utils/cookies.ts
+++
@@ -1,6 +0,0 @@
-import { createCookie } from "@remix-run/node";
-
-export const idTokenCookie = createCookie("idToken");
-export const accessTokenCookie = createCookie("accessToken");
-export const accessKeyIdCookie = createCookie("accessToken");
-export const secretAccessKeyCookie = createCookie("accessToken");
