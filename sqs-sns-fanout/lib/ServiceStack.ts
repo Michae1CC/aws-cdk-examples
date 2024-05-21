f1da6ac448b84339a0cb5d0b1e3a74f63c53336c
Michael CC <333mmcc@gmail.com>
2024-05-21 22:56:02 +1000

    Add icons bucket

--- sqs-sns-fanout/lib/ServiceStack.ts
+++ sqs-sns-fanout/lib/ServiceStack.ts
@@ -1,8 +1,17 @@
 import * as cdk from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class ServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const iconsBucket = new s3.Bucket(this, "iconsBucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
