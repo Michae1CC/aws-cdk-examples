cadffb7686b2465151fef6a047adfe147f3a1be1
Michael CC <333mmcc@gmail.com>
2024-05-26 17:00:56 +1000

    ap working

--- sqs-sns-fanout/lib/ServiceStack.ts
+++ sqs-sns-fanout/lib/ServiceStack.ts
@@ -10,13 +10,32 @@ export class ServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const iconsBucket = new s3.Bucket(this, "iconsBucket", {
+    const graphicsBucket = new s3.Bucket(this, "graphicsBucket", {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,
       versioned: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    // Delegate access control to the access point
+    graphicsBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        sid: "DelegateAccessToAccessPoint",
+        effect: iam.Effect.ALLOW,
+        actions: ["*"],
+        principals: [new iam.AnyPrincipal()],
+        resources: [
+          graphicsBucket.bucketArn,
+          graphicsBucket.arnForObjects("*"),
+        ],
+        conditions: {
+          StringEquals: {
+            "s3:DataAccessPointAccount": this.account,
+          },
+        },
+      })
+    );
 
     const designUserPassword = new secretsmanager.Secret(this, "userPassword", {
       generateSecretString: {
@@ -31,43 +50,35 @@ export class ServiceStack extends cdk.Stack {
       passwordResetRequired: false,
     });
 
-    const DESIGN_USER_S3_ACCESS_POINT_NAME = "designUserAccessPoint" as const;
+    const DESIGN_USER_S3_ACCESS_POINT_NAME = "design-ap" as const;
     const designUserAccessPointArn =
       `arn:aws:s3:${this.region}:${this.account}:accesspoint/${DESIGN_USER_S3_ACCESS_POINT_NAME}` as const;
+
+    const accessPointPolicyDocument = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          sid: "AllowDesignUserGetAndPut",
+          effect: iam.Effect.ALLOW,
+          actions: ["s3:ListBucket", "s3:GetObject", "s3:PutObject"],
+          principals: [new iam.ArnPrincipal(designUser.userArn)],
+          // When specifying the bucket objects in the access point resource
+          // policy, the object resource must always be prepended with /object
+          resources: [
+            designUserAccessPointArn,
+            `${designUserAccessPointArn}/object/*`,
+          ],
+        }),
+      ],
+    });
 
     const designUserAccessPoint = new s3.CfnAccessPoint(
       this,
       "designerAccessPoint",
       {
-        bucket: iconsBucket.bucketName,
-        policy: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              sid: "AllowDesignUserGetAndPut",
-              effect: iam.Effect.ALLOW,
-              actions: ["s3:ListObjects", "s3:GetObject", "s3:PutObject"],
-              principals: [new iam.ArnPrincipal(designUser.userArn)],
-              resources: [`${designUserAccessPointArn}/*`],
-            }),
-          ],
-        }),
+        bucket: graphicsBucket.bucketName,
+        name: DESIGN_USER_S3_ACCESS_POINT_NAME,
+        policy: accessPointPolicyDocument,
       }
-    );
-
-    // Delegate access control to the access point
-    iconsBucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        sid: "DelegateAccessToAccessPoint",
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:ListObjects", "s3:GetObject", "s3:PutObject"],
-        principals: [new iam.AnyPrincipal()],
-        resources: [iconsBucket.bucketArn, iconsBucket.arnForObjects("*")],
-        conditions: {
-          StringEquals: {
-            "s3:DataAccessPointAccount": this.account,
-          },
-        },
-      })
     );
   }
 }
