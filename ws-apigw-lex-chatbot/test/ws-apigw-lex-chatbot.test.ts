2145582b5da932907144e747e3fbf353b2d60359
Michael CC <333mmcc@gmail.com>
2024-07-07 19:01:06 +1000

    started ws-apigw-lex-chatbot

---
+++ ws-apigw-lex-chatbot/test/ws-apigw-lex-chatbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WsApigwLexChatbot from '../lib/ws-apigw-lex-chatbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ws-apigw-lex-chatbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WsApigwLexChatbot.WsApigwLexChatbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
