7064e998b85a4121084f1d8d1314dadb945fae1b
Michael CC <333mmcc@gmail.com>
2024-09-07 23:41:44 +1000

    Add ecs-anywhere

---
+++ ecs-anywhere/test/ecs-anywhere.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsAnywhere from '../lib/ecs-anywhere-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-anywhere-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsAnywhere.EcsAnywhereStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
