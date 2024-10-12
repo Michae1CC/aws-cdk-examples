85665c157fe6a69b194cbc8657a45bfac51052c2
Michael CC <333mmcc@gmail.com>
2024-10-12 14:10:08 +1000

    Added code-deploy

---
+++ code-deploy/test/code-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodeDeploy from '../lib/code-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/code-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodeDeploy.CodeDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
