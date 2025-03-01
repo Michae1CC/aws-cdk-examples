c3a4e1f8275a45c52f373d5486fe3ac1c5a0c72d
Michael CC <333mmcc@gmail.com>
2025-03-01 18:49:16 +1000

    ex1-2 fixes

--- ans-c01-ex/README.md
+++ ans-c01-ex/README.md
@@ -1,5 +1,74 @@
 # Welcome to your CDK TypeScript project
 
+Attempting the call from instance a fails
+
+From instance b
+
+```
+aws kms list-keys
+{
+    "Keys": [
+        {
+            "KeyId": "518efc3e-d0e3-4a62-8356-acefdb91cc7d",
+            "KeyArn": "arn:aws:kms:us-east-1:202533509701:key/518efc3e-d0e3-4a62-8356-acefdb91cc7d"
+        },
+        ...
+    ]
+}
+```
+
+```
+[ec2-user@ip-10-0-95-181 ~]$ dig kms.us-east-1.amazonaws.com
+
+; <<>> DiG 9.18.33 <<>> kms.us-east-1.amazonaws.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 15307
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
+
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 4096
+;; QUESTION SECTION:
+;kms.us-east-1.amazonaws.com.   IN      A
+
+;; ANSWER SECTION:
+kms.us-east-1.amazonaws.com. 52 IN      A       67.220.241.181
+
+;; Query time: 0 msec
+;; SERVER: 10.0.0.2#53(10.0.0.2) (UDP)
+;; WHEN: Fri Feb 28 12:47:00 UTC 2025
+;; MSG SIZE  rcvd: 72
+```
+
+```
+[ec2-user@ip-10-1-167-200 ~]$ dig kms.us-east-1.amazonaws.com
+
+; <<>> DiG 9.18.33 <<>> kms.us-east-1.amazonaws.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 17377
+;; flags: qr rd ra; QUERY: 1, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 1
+
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 4096
+;; QUESTION SECTION:
+;kms.us-east-1.amazonaws.com.   IN      A
+
+;; ANSWER SECTION:
+kms.us-east-1.amazonaws.com. 60 IN      A       10.1.58.47
+kms.us-east-1.amazonaws.com. 60 IN      A       10.1.139.164
+
+;; Query time: 0 msec
+;; SERVER: 10.1.0.2#53(10.1.0.2) (UDP)
+;; WHEN: Fri Feb 28 12:46:42 UTC 2025
+;; MSG SIZE  rcvd: 88
+```
+
+The following hangs, could be transitivity issues
+```
+[ec2-user@ip-10-0-95-181 ~]$ aws kms list-keys --endpoint-url https://10.1.58.47
+```
+
 This is a blank project for CDK development with TypeScript.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
--- ans-c01-ex/lib/ans-c01-ex-stack.ts
+++ ans-c01-ex/lib/ans-c01-ex-stack.ts
@@ -1,4 +1,4 @@
-import { aws_ec2 as ec2 } from "aws-cdk-lib";
+import { aws_ec2 as ec2, aws_iam as iam, aws_kms as kms } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
@@ -32,6 +32,117 @@ export class Ex1_2Stack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
+    });
+
+    const vpcAVpcBPeeringConnection = new ec2.CfnVPCPeeringConnection(
+      this,
+      "vpc-a-vpc-b-peer",
+      {
+        peerVpcId: vpcA.vpcId,
+        vpcId: vpcB.vpcId,
+        peerOwnerId: props.env?.region,
+      },
+    );
+
+    // Add the CIDR range of VPC A to VPC B and vice-versa
+    vpcB
+      .selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_ISOLATED })
+      .subnets.forEach((subnet, index) => {
+        new ec2.CfnRoute(this, `vpc-a-vpc-b-peer-${index}`, {
+          routeTableId: subnet.routeTable.routeTableId,
+          destinationCidrBlock: "10.0.0.0/16",
+          vpcPeeringConnectionId: vpcAVpcBPeeringConnection.attrId,
+        });
+      });
+
+    vpcA
+      .selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_ISOLATED })
+      .subnets.forEach((subnet, index) => {
+        new ec2.CfnRoute(this, `vpc-b-peer-${index}`, {
+          routeTableId: subnet.routeTable.routeTableId,
+          destinationCidrBlock: "10.1.0.0/16",
+          vpcPeeringConnectionId: vpcAVpcBPeeringConnection.attrId,
+        });
+      });
+
+    // Create an interface endpoint for the KMS service
+    const kmsInterfaceEndPoint = new ec2.InterfaceVpcEndpoint(
+      this,
+      "kms-endpoint",
+      {
+        service: ec2.InterfaceVpcEndpointAwsService.KMS,
+        vpc: vpcB,
+        subnets: {
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      },
+    );
+
+    kmsInterfaceEndPoint.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.AnyPrincipal()],
+        resources: ["*"],
+        actions: ["kms:ListKeys", "kms:ListAliases"],
+      }),
+    );
+
+    // Create a security group for the instance connect endpoint for VPC a
+    const instanceConnectASg = new ec2.SecurityGroup(
+      this,
+      "instance-connect-vpc-a",
+      {
+        vpc: vpcA,
+        allowAllOutbound: true,
+      },
+    );
+
+    instanceConnectASg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.icmpPing(),
+      "Allow pings from any connection",
+    );
+    instanceConnectASg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.SSH,
+      "Allow SSH from any connection",
+    );
+
+    // Create a instance connect for VPC A
+    new ec2.CfnInstanceConnectEndpoint(this, "vpc-a-instance-connect", {
+      subnetId: vpcA.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }).subnetIds[0],
+      securityGroupIds: [instanceConnectASg.securityGroupId],
+    });
+
+    // Create a security group for the instance connect endpoint for VPC b
+    const instanceConnectBSg = new ec2.SecurityGroup(
+      this,
+      "instance-connect-vpc-b",
+      {
+        vpc: vpcB,
+        allowAllOutbound: true,
+      },
+    );
+
+    instanceConnectBSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.icmpPing(),
+      "Allow pings from any connection",
+    );
+    instanceConnectBSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.SSH,
+      "Allow SSH from any connection",
+    );
+
+    // Create a instance connect for VPC B
+    new ec2.CfnInstanceConnectEndpoint(this, "vpc-b-instance-connect", {
+      subnetId: vpcB.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }).subnetIds[0],
+      securityGroupIds: [instanceConnectBSg.securityGroupId],
     });
   }
 }
