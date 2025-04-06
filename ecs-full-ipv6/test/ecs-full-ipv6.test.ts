5e7f7d1e1b0458d26bb797beb8a7f6734c5f259d
Michael CC <333mmcc@gmail.com>
2025-04-06 16:49:55 +1000

    Add full ipv6

---
+++ ecs-full-ipv6/test/ecs-full-ipv6.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsFullIpv6 from '../lib/ecs-full-ipv6-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-full-ipv6-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsFullIpv6.EcsFullIpv6Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
