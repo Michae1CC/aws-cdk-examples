be0a77e424324d780e16fef5cede959283952348
Michael CC <333mmcc@gmail.com>
2025-04-13 21:26:15 +1000

    Add to README

--- ecs-full-ipv6/lib/ecs-full-ipv6-stack.ts
+++ ecs-full-ipv6/lib/ecs-full-ipv6-stack.ts
@@ -1,16 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class EcsFullIpv6Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsFullIpv6Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
