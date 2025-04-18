215d4f26e1130f2e29034fbb732b96d3a3a0ffa4
Michael CC <333mmcc@gmail.com>
2025-04-18 23:28:20 +1000

    Add vpc

--- ecs-full-ipv6/lib/ecs-full-ipv6-stack.ts
+++ ecs-full-ipv6/lib/ecs-full-ipv6-stack.ts
@@ -1,8 +1,31 @@
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class EcsFullIpv6Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "vpc", {
+      createInternetGateway: true,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      ipProtocol: ec2.IpProtocol.DUAL_STACK,
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 20,
+          ipv6AssignAddressOnCreation: true,
+        },
+        {
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 20,
+          ipv6AssignAddressOnCreation: true,
+        },
+      ],
+    });
   }
 }
