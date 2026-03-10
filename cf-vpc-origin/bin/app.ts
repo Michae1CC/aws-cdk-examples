7bbc8af51c8df5babd10bf7006d411a3596d51a0
Michael C-C <35992239+Michae1CC@users.noreply.github.com>
2026-03-10 18:14:07 +1000

    Update app.ts

--- cf-vpc-origin/bin/app.ts
+++ cf-vpc-origin/bin/app.ts
@@ -8,20 +8,17 @@ const app = new cdk.App();
 const dnsStack = new DnsStack(app, "dns-stack", {
   env: {
     region: "us-east-1",
-    account: "786511284175",
   },
 });
 const service = new ServiceStack(app, "service-stack", {
   env: {
     region: "us-east-1",
-    account: "786511284175",
   },
   hostedZone: dnsStack.hostedZone,
 });
 new DistributionStack(app, "distribution-stack", {
   env: {
     region: "us-east-1",
-    account: "786511284175",
   },
   hostedZone: dnsStack.hostedZone,
   privateAlb: service.privateAlb,
