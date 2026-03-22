d3d50342efcd18e3c5b2469928483c253dee8a5f
Michael CC <333mmcc@gmail.com>
2026-03-22 22:30:09 +1000

    Add ec2-ebs

---
+++ ec2-ebs/test/ec2-ebs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2Ebs from '../lib/ec2-ebs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2-ebs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2Ebs.Ec2EbsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
