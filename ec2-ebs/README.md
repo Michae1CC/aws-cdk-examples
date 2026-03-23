d38533a0c0efee24d70f55f87c7e368371b42cb0
Michael CC <333mmcc@gmail.com>
2026-03-23 22:50:37 +1000

    Add CDK

--- ec2-ebs/README.md
+++ ec2-ebs/README.md
@@ -12,3 +12,10 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+## References
+
+* <https://docs.aws.amazon.com/ebs/latest/userguide/ebs-using-volumes.html>
+* <https://docs.aws.amazon.com/ebs/latest/userguide/recognize-expanded-volume-linux.html>
+* <https://www.youtube.com/watch?v=dvRYgB3T6d4>
+* <https://youtu.be/2DrjQBL5FMU?si=IMuBcZBatvTqCJJE>
--- ec2-ebs/lib/ec2-ebs-stack.ts
+++ ec2-ebs/lib/ec2-ebs-stack.ts
@@ -1,16 +1,104 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_ec2 as ec2, aws_iam as iam, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export class Ec2EbsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+const VPC_CIDR = "10.0.0.0/16" as const;
+
+export class Ec2EbsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "vpc", {
+      ipProtocol: ec2.IpProtocol.IPV4_ONLY,
+      maxAzs: 2,
+      natGateways: 1,
+      createInternetGateway: true,
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      ipAddresses: ec2.IpAddresses.cidr(VPC_CIDR),
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ec2EbsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const instanceSg = new ec2.SecurityGroup(this, "instance-sg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+    });
+
+    instanceSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.icmpPing(),
+      "Allow pings from any connection",
+    );
+
+    instanceSg.addIngressRule(
+      ec2.Peer.ipv4(VPC_CIDR),
+      ec2.Port.SSH,
+      "Allow SSH from VPC",
+    );
+
+    new ec2.CfnInstanceConnectEndpoint(this, "instance-connect", {
+      subnetId: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      }).subnetIds[0],
+      securityGroupIds: [instanceSg.securityGroupId],
+    });
+
+    // Create IAM role for the instance
+    const instanceRole = new iam.Role(this, "instance-role", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "AmazonSSMManagedInstanceCore",
+        ),
+      ],
+    });
+
+    const instance = new ec2.Instance(this, "instance", {
+      vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      }),
+      associatePublicIpAddress: false,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.D3,
+        ec2.InstanceSize.XLARGE2,
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023({
+        cpuType: ec2.AmazonLinuxCpuType.X86_64,
+      }),
+      securityGroup: instanceSg,
+      role: instanceRole,
+      ebsOptimized: true,
+      blockDevices: [
+        {
+          deviceName: "/dev/xvda", // Root volume
+          volume: ec2.BlockDeviceVolume.ebs(20, {
+            volumeType: ec2.EbsDeviceVolumeType.GP3,
+            iops: 3000,
+            throughput: 125,
+            encrypted: true,
+            deleteOnTermination: true,
+          }),
+        },
+        {
+          deviceName: "/dev/xvdb", // Additional data volume
+          volume: ec2.BlockDeviceVolume.ebs(100, {
+            volumeType: ec2.EbsDeviceVolumeType.GP3,
+            iops: 4000,
+            throughput: 250,
+            encrypted: true,
+            deleteOnTermination: false, // Preserve data volume
+          }),
+        },
+      ],
+    });
   }
 }
